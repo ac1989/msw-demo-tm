2ddedfdbc48378071eb8f2e7aa9acd14c4e98928
ac9189 <welladjusted220@gmail.com>
2021-01-16 18:06:19 +0000

    Add basic functionality

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { useQuery } from "react-query";
-import { fetchMovies } from "./api/fetchMovies";
-import "./Movies.css";
-import { Movie } from "./Movie";
+import React from 'react'
+import { fetchMovies } from './api/fetchMovies'
+import './Movies.css'
+import { Movie } from './Movie'
 
 export function MovieCard({ movie, handleClick }) {
-  const { title, poster } = movie;
+  const { title, poster } = movie
   return (
     <div
       className="movie-card-poster"
@@ -14,34 +13,39 @@ export function MovieCard({ movie, handleClick }) {
     >
       <span>{title}</span>
     </div>
-  );
+  )
 }
 
 export function Movies() {
-  const { status, data } = useQuery("movies", fetchMovies);
-  const [selectedMovieIndex, setSelectedMovieIndex] = React.useState(-1);
+  const [selectedMovieIndex, setSelectedMovieIndex] = React.useState(-1)
+  const [{ status, movies }, setState] = React.useState({
+    status: 'loading',
+    movies: null,
+  })
+
+  React.useEffect(() => {
+    fetchMovies().then((movies) => setState({ status: 'success', movies }))
+  }, [])
+
+  function handleMovieUpdate(movie) {
+    const movieIndex = movies.findIndex((item) => item.id === movie.id)
+    setState({
+      status,
+      movies: [
+        ...movies.slice(0, movieIndex),
+        movie,
+        ...movies.slice(movieIndex + 1),
+      ],
+    })
+  }
 
   return (
     <div className="movies-page">
-      {status === "success" && selectedMovieIndex >= 0 && (
-        <>
-          <nav
-            style={{ width: "100%", maxWidth: "960px", marginBottom: "24px" }}
-          >
-            <button onClick={() => setSelectedMovieIndex(-1)}>
-              {"< Back"}
-            </button>
-          </nav>
+      {status === 'loading' && <p>loading...</p>}
 
-          <Movie movie={data[selectedMovieIndex]} />
-        </>
-      )}
-
-      {status === "loading" && <p>loading...</p>}
-
-      {status === "success" && selectedMovieIndex === -1 && (
+      {status === 'success' && selectedMovieIndex === -1 && (
         <section className="movies-selector">
-          {data.map((movie, i) => (
+          {movies.map((movie, i) => (
             <span className="hover-effect">
               <MovieCard
                 key={movie.id}
@@ -52,6 +56,23 @@ export function Movies() {
           ))}
         </section>
       )}
+
+      {status === 'success' && selectedMovieIndex >= 0 && (
+        <>
+          <nav
+            style={{ width: '100%', maxWidth: '960px', marginBottom: '24px' }}
+          >
+            <button onClick={() => setSelectedMovieIndex(-1)}>
+              {'< Back'}
+            </button>
+          </nav>
+
+          <Movie
+            movie={movies[selectedMovieIndex]}
+            onMovieUpdate={handleMovieUpdate}
+          />
+        </>
+      )}
     </div>
-  );
+  )
 }
